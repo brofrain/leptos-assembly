d6ae86ab5a67acc952c0e148e4f72622059fd68a
brofrain <knwelc@gmail.com>
2023-11-01 18:51:06 +0100

    chore

--- src/env.rs
+++ src/env.rs
@@ -3,4 +3,4 @@ use std::sync::LazyLock;
 pub static CARGO_PKG_NAME: LazyLock<String> =
     LazyLock::new(|| env!("CARGO_PKG_NAME").replace('-', "_"));
 
-pub const APP_REPOSITORY_URL: &str = env!("APP_REPOSITORY_URL");
+pub const PROJECT_REPOSITORY_URL: &str = env!("PROJECT_REPOSITORY_URL");
