3b021e8d34758cf921a28c9041194951ea40e65a
brofrain <knwelc@gmail.com>
2023-11-03 21:27:27 +0100

    chore: dead code

--- src/env.rs
+++
@@ -1,6 +0,0 @@
-use std::sync::LazyLock;
-
-pub static CARGO_PKG_NAME: LazyLock<String> =
-    LazyLock::new(|| env!("CARGO_PKG_NAME").replace('-', "_"));
-
-pub const PROJECT_REPOSITORY_URL: &str = env!("PROJECT_REPOSITORY_URL");
