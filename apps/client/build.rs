146f230156fb42d692e6df3ba712922e3b0c457c
brofrain <knwelc@gmail.com>
2023-12-31 12:31:39 +0100

    fix: explicit rerun vite prebuild

--- apps/client/build.rs
+++ apps/client/build.rs
@@ -1,6 +1,12 @@
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-changed=app");
+    println!("cargo:rerun-if-changed=components");
+    println!("cargo:rerun-if-changed=components");
+    println!("cargo:rerun-if-changed=layouts");
+    println!("cargo:rerun-if-changed=pages");
+
     if !cfg!(feature = "vite-prebuild") {
         return;
     }
