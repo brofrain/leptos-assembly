1d522b590882ec6cf50f8ce165f06ae022c078bf
brofrain <knwelc@gmail.com>
2024-02-12 22:01:59 +0100

    feat: snapshot test

--- packages/client/components/base/button_test.rs
+++ packages/client/components/base/button_test.rs
@@ -5,7 +5,7 @@ use wasm_bindgen_test::wasm_bindgen_test;
 use crate::BaseButton;
 
 #[wasm_bindgen_test]
-async fn match_snapshot() {
+async fn render_children() {
     mock_browser();
 
     leptos::mount_to_body(
@@ -15,7 +15,18 @@ async fn match_snapshot() {
     let el = document().query_selector("button").unwrap().unwrap();
 
     let msg = el.text_content().unwrap();
-    let html = el.outer_html();
-
     assert_eq!(&msg, "Hello, World!");
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[test]
+fn match_snapshot() {
+    use dev::vendor::insta::assert_yaml_snapshot;
+    use leptos::ssr::render_to_string;
+
+    let html = render_to_string(|| {
+        view! { <BaseButton>{"Hello, World!"}</BaseButton> }.into_view()
+    });
+
+    assert_yaml_snapshot!(html);
 }
