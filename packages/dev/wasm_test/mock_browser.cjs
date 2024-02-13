2e913fbee3196d491426968a53abeb7a40e35707
brofrain <knwelc@gmail.com>
2024-02-13 19:56:43 +0100

    fix: teardown mocked browser

--- packages/dev/wasm_test/mock_browser.cjs
+++ packages/dev/wasm_test/mock_browser.cjs
@@ -1,6 +1,10 @@
 const { GlobalRegistrator } = require("@happy-dom/global-registrator");
 
 exports.mock_browser = () => {
+  if (global.document) {
+    global.document.body.innerHTML = "";
+    GlobalRegistrator.unregister();
+  }
   GlobalRegistrator.register();
   Window = Object;
 };
